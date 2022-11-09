a25765c3164801685bd6fef596f40719524b6652
Batuhan Budak <batuhan.budak@gmail.com>
2022-11-09 23:16:01 +0300

    Fix grid gap

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Grid = ({ title, children, className }: Props) => (
   <div className={className}>
     <h2 className="text-xl font-bold pb-4">{title}</h2>
-    <div className="grid grid-cols-auto-fill gap-8">{children}</div>
+    <div className="grid grid-cols-auto-fill gap-9">{children}</div>
   </div>
 );
 
